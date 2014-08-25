050731a207ade4725df7a30d85d0cfbecf6f30e4
Maryam Rabiee <rabiei.maryam@gmail.com>
2014-08-26 00:33:30 +0430

    calcute accuracy changed

--- calcute_acuracy.c
+++ calcute_acuracy.c
@@ -10,8 +10,6 @@
 #include <inttypes.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-//#include "align_bisulfite.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,66 +20,65 @@ int main(int argc, char *argv[]) {
     samFile = fopen("a.sam", "r");
     long long badAlignedReads = 0;
     long long notAlignedReads = 0;
-    long long readNum;
-char line[1000];
-int header = 1;
-char *qname, *rnext, *pnext, *seq_string, *quality_string,*rname, *cigar;
-int flag, i;
-uint64_t pos;
-uint32_t mapq;
-long long int tlen;
+    long long readNum = 0;
+    char line[1000];
+    int header = 1;
+    char *qname, *rnext, *pnext, *seq_string, *quality_string,*rname, *cigar;
+    int flag, i;
+    uint64_t pos;
+    uint32_t mapq;
+    long long int tlen;
     rname = malloc(100 * sizeof(char));
     cigar = malloc(200 * sizeof(char));
-qname = malloc(100 * sizeof(char));
-rnext = malloc(100 * sizeof(char));
-pnext = malloc(100 * sizeof(char));
-seq_string = malloc(1000 * sizeof(char));
-quality_string = malloc(500 * sizeof(char));
+    qname = malloc(100 * sizeof(char));
+    rnext = malloc(100 * sizeof(char));
+    pnext = malloc(100 * sizeof(char));
+    seq_string = malloc(1000 * sizeof(char));
+    quality_string = malloc(500 * sizeof(char));
 
-int stop = 0;
-while (1) {
-        if (fgets(line, 1000, samFile) == NULL) {
-            break;
-        }
-        while (line[0] == '@'){
-            if(fgets(line, 1000, samFile) == NULL){
-                stop = 1;
+    int stop = 0;
+    while (1) {
+            if (fgets(line, 1000, samFile) == NULL) 
                 break;
+            while (line[0] == '@'){
+                if(fgets(line, 1000, samFile) == NULL){
+                    stop = 1;
+                    break;
+                }
             }
-        }
-        if(stop)
-            break;
-        readNum++;
-        sscanf(line,"%s\t%d\t%s\t%"PRIu64"\t%u\t%s\t%s\t%s\t%lld\t%s\t%s\n",qname, &flag, rname, &pos,&mapq, cigar,rnext,pnext, &tlen,seq_string,quality_string);
-        char* tokens=strtok(qname, ":");
-        tokens = strtok(NULL, ":");
+            if(stop)
+                break;
+            readNum++;
+            sscanf(line,"%s\t%d\t%s\t%"PRIu64"\t%u\t%s\t%s\t%s\t%lld\t%s\t%s\n",qname, &flag, rname, &pos,&mapq, cigar,rnext,pnext, &tlen,seq_string,quality_string);
+            char* tokens=strtok(qname, ":");
+            tokens = strtok(NULL, ":");
     
-        char *first,*second;
-        first = strtok(tokens, "-");
-        second = strtok(NULL, "-");
+            char *first,*second;
+            first = strtok(tokens, "-");
+            second = strtok(NULL, "-");
     
-        //printf( "f: %s   sec:%s \n",first,second  );
-    uint64_t start, end;
-    start = strtoll(first,NULL,10);
-    end = strtoll(second,NULL,10);
-    printf( "first: %" PRIu64 "   sec:%" PRIu64 " \n",start,end  );
-    if(start >= pos-20 && pos <=end+20);
-        //exact aligning
-    else
-        badAlignedReads += 1;
+            printf( "f: %s   sec:%s \n",first,second  );
+            uint64_t start, end;
+            start = strtoll(first,NULL,10);
+            end = strtoll(second,NULL,10);
+            printf( "first: %" PRIu64 "   sec:%" PRIu64 " \n",start,end  );
+            if(start-20 <= pos && pos <=end+20);
+                //exact aligning
+            else if(!strstr(cigar,"*"))
+                badAlignedReads += 1;
     //readCigar(cigar, pos, seq_string, i,start ,end);
 
-    fprintf(stdout, "%s\t%d\t%s\t%"PRIu64"\t%u\t%s\t%s\t%s\t%lld\t%s\t%s\n \n",qname, flag, rname, pos,mapq, cigar,rnext,pnext, tlen,seq_string,quality_string);
-    
-}
-        if(strstr(cigar,"*"))
-            notAlignedReads++;
-        fprintf(stdout, "number of not aligned reads: %ull \n number of reads with wrong alignment : %ull \n total reads : %ull \n percentage :%f \n",notAlignedReads, badAlignedReads, readNum , badAlignedReads/readNum * 100);
+            fprintf(stdout, "%s\t%d\t%s\t%"PRIu64"\t%u\t%s\t%s\t%s\t%lld\t%s\t%s\n \n",qname, flag, rname, pos,mapq, cigar,rnext,pnext, tlen,seq_string,quality_string);
 
+            fprintf(stdout,"\n cigar %s \n ",cigar);
+            if(strstr(cigar,"*"))
+                notAlignedReads++;
+    }
+    float accuracy = ((float)badAlignedReads/readNum)*100.0 ;
+    fprintf(stdout, "number of not aligned reads: %lld \n number of reads with wrong alignment : %lld \n total reads : %lld \n percentage of bad aligned reads :%10f \n",notAlignedReads, badAlignedReads, readNum , accuracy);
     fclose(samFile);
     
 }
-
 void readCigar(char * cigar, uint64_t ref_i, char *seq_string, long readNum ) {
 	//fprintf(stderr, "salam\n");
 	int pos = 0;
