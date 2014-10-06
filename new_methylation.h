27cf150de0c8af05a2a9ec16c6b15549e4928966
Maryam Rabiee <rabiei.maryam@gmail.com>
2014-10-06 22:21:09 +0330

    new methylation code

---
+++ new_methylation.h
@@ -0,0 +1,14 @@
+//
+//  new_methylation.h
+//  aryana
+//
+//  Created by Maryam Rabiee on 10/6/14.
+//  Copyright (c) 2014 Maryam Rabiee. All rights reserved.
+//
+
+#ifndef __aryana__new_methylation__
+#define __aryana__new_methylation__
+
+#include <iostream>
+
+#endif /* defined(__aryana__new_methylation__) */
