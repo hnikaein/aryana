b004f02df4ca5682f93b4a18ccf28d34fc5a9e37
MiladInk <mi.aghajohari@gmail.com>
2019-06-25 17:15:04 +0430

    add printing

--- read_simul.cpp
+++ read_simul.cpp
@@ -50,7 +50,11 @@ long long pairMinDis = 300, pairMaxDis = 1000; // Minimum and maximum distance b
 char bases[4] = {'A', 'C', 'G', 'T'};
 
 long long get_global_pose_from_chromNum_and_index(string chromNumStr, long long local_position) {
-    int chromIdx = chromIndex.at(chromNumStr);
+    try {
+        int chromIdx = chromIndex.at(chromNumStr);
+    }catch {
+        cerr << "chromeNumStr:" << chromNumStr << endl;
+    }
     long long global_position = 0;
     for (int i = 0; i < chromIdx; i++) {
         global_position += chromLen[i];
