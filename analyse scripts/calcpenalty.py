997723829f14f1c0c0c36862fa5eb82b09309151
Hassan Nikaein <nikaein2@gmail.com>
2023-05-04 17:15:12 +0330

    [CHANGE] analyse scripts

--- analyse scripts/calcpenalty.py
+++ analyse scripts/calcpenalty.py
@@ -2,13 +2,15 @@
 # RealPos CorrectPos      RealMismatch    RealGapOpen     RealGapExt      SamSeq  AlnCIGAR        AlnSeq  RealCIGAR
 # RealSeq
 
-algos = ["aryana", "bwameth"]
+algos = ["aryana_old", "bsmap"]
 algos_len = len(algos)
 samples = [
-    #    "/home/groups/aryanabis/methyl/outputs/result-hg38_real_read_liver_small2.fastq___200________/",
-    "/home/groups/aryanabis/methyl/outputs/result-hg38_real_read_liver_small.fastq___200________/",
-    "/home/groups/aryanabis/methyl/outputs/result-hg38_real_read_placenta_small.fastq___200________/",
+    # "/home/groups/aryanabis/methyl/outputs/result-hg38_real_read_liver_small2.fastq___200________/",
+    # "/home/groups/aryanabis/methyl/outputs/result-hg38_real_read_liver_small.fastq___200________/",
+    # "/home/groups/aryanabis/methyl/outputs/result-hg38_real_read_placenta_small.fastq___200________/",
+    "/home/groups/aryanabis/methyl/outputs/result-hg38_real_read_SRR19154020___150_1_______/",
 ]
+paired_end = True
 for sample in samples:
     reads = {}
     differention = {}
@@ -22,8 +24,8 @@ for sample in samples:
             if len(parts) < 7:
                 break
             read_name = parts[0]
-            if not read_name in reads:
-                reads[read_name] = [-1] * algos_len
+            if read_name not in reads:
+                reads[read_name] = [[], []]  # * algos_len
             if int(parts[1]) == 0:
                 continue
             elif int(parts[1]) != 1:
@@ -33,21 +35,31 @@ for sample in samples:
             ge = int(parts[7])
             mat = int(parts[4]) - mis - go - ge
             pen = 4 * mis + 6 * go + 1 * ge - 4 * mat
-            reads[read_name][algo_i] = pen
+            if len(reads[read_name][algo_i]) > 0 and reads[read_name][algo_i][0][1] != parts[2]:
+                continue
+            reads[read_name][algo_i].append((pen, parts[2]))
         f.close()
-    unique_alignment = [0] * algos_len
+    unique_alignment = [0, 0]  # * algos_len
     for read_name, penalties in reads.items():
-        for i in range(algos_len):
-            if penalties[i] == -1:
-                for j in range(algos_len):
-                    if penalties[j] != -1:
-                        unique_alignment[j] += 1
-                break
+        if paired_end:
+            for i in range(2):
+                if len(penalties[i]) < 2:
+                    penalties[i] = []
+        diff_len = len(penalties[0]) - len(penalties[1])
+        if diff_len > 0:
+            unique_alignment[0] += 2
+        elif diff_len < 0:
+            unique_alignment[1] += 2
         else:
-            dif = penalties[1] - penalties[0]
-            if dif not in differention:
-                differention[dif] = 1
-            else:
-                differention[dif] += 1
+            penalties[0].sort()
+            penalties[1].sort()
+            for i in range(min(len(penalties[0]), len(penalties[1]))):
+                dif = penalties[1][i][0] - penalties[0][i][0]
+                if dif < -900:
+                    print(dif, read_name)
+                if dif not in differention:
+                    differention[dif] = 1
+                else:
+                    differention[dif] += 1
     print(sorted(differention.items()))
     print(unique_alignment)
